3009f4c1b1fa51627af38774259172cb67520f72
Julia-BARGOIN <julia.bargoin@tuta.io>
2020-10-15 14:56:01 +0200

    chore(MyReact): initialize projet

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.scss';
+
+const App = () => <div></div>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
